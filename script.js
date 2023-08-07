e73228a60042c6fa47cbc87c00f67fd30678d2b0
Blake-Stephenson <49764055+Blake-Stephenson@users.noreply.github.com>
2023-08-07 01:48:18 +0000

    add db fetch

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 import { HexGrid, Mask, Bar, Hitbox } from "./src/GUI.js";
 
+//fetch board data from server
+fetch("https://zoory-db.epicblake8.repl.co/data")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Process the response data
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Fetch error:", error);
+  });
 
 // Create the PIXI application and set its properties
 const app = new PIXI.Application({
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x420420,
-	width: 1280,
-	height: 720
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0x420420,
+  width: 1280,
+  height: 720
 });
 // Add the PIXI application's view (canvas) to the container
 document.body.appendChild(app.view);
@@ -17,7 +32,7 @@ const bar = new Bar(app.screen);
 
 //create board, contains grid
 const board = new PIXI.Container();
-let grid = new HexGrid(4,8,bar,app.screen);
+let grid = new HexGrid(20, 40, bar, app.screen);
 board.addChild(grid.getGrid());
 
 //create game hitbox
